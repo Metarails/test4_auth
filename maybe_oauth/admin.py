d53c91d3e5f82f6a528e01b6358bafe40014b674
Metarails <SamiSantamaa87@gmail.com>
2023-06-01 11:38:37 +0300

    installing rest and first model

--- maybe_oauth/admin.py
+++ maybe_oauth/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import CoolBeans
+
 # Register your models here.
+admin.site.register(CoolBeans)
--- maybe_oauth/models.py
+++ maybe_oauth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class CoolBeans(models.Model):
+    """class for making wowzers table for database"""
+
+    something = models.CharField(max_length=255, default="cool stuff bro")
